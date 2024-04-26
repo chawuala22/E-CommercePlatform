49394cd3aec219a2b2fd6eb8b0d6591b6b155ad4
Mrojas <miguel.rojas@castor.com.co>
2024-04-26 10:40:45 -0500

    fix responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private _authServie: AuthService, private router: Router) { }
+
+  logout() {
+    this.router.navigate(['/login']);
+    this._authServie.logout();
+    Swal.fire('Logout successfully')
+  }
 }
